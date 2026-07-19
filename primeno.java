4da146d481faf2be9ebaa324fe82f89c42969e97
1s-debug <sharvari.wadekar2203@gmail.com>
2026-07-19 13:23:50 +0530

    Added Hard_que folder

--- primeno.java
+++ primeno.java
@@ -1,8 +1,8 @@
 public class primeno {
     public static void main(String[] args) {
-        int no=100;
+        int no=9;
         int count=0;
-        for( int i=2; i*i<= no; i++)
+        for( int i=1; i<= no; i++)
         {
             if(no%i==0)            //9%1==0 yes  count=1 
             {                      //9%2==0 no,  9%3==0 yess  count=2
